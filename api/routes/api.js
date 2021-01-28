6c25946df36e4bf30cdc50b444873ef75b921250
Debian <bitnami@ip-172-26-9-162.ec2.internal>
2021-01-28 00:24:43 +0000

    project architecture

---
+++ api/routes/api.js
@@ -0,0 +1,13 @@
+var express = require(“express”);
+
+var router = express.Router();
+
+
+router.get(“/”, function(req, res, next) {
+
+    res.send(“API is working properly”);
+
+});
+
+
+module.exports = router;
