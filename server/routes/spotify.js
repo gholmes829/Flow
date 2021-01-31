64c85e9b85a1c3e5cf346362275d15aae1b57610
gholmesss <g.holmes429@gmail.com>
2021-01-30 18:22:19 -0600

    moved routers to server/routes

---
+++ server/routes/spotify.js
@@ -0,0 +1,129 @@
+var express = require('express');
+var request = require('request');
+var querystring = require('querystring');
+
+var router = express.Router();
+
+//Spotify API
+var client_id = 'c7cca4bb63634ddf8e6d205c9b23a7b6'; // Your client id
+var client_secret = '849cf7b45dd849ed97ec6990bb6896a6'; // Your secret
+var redirect_uri = 'http://catchthatflow.com:9000/callback/';
+var stateKey = 'spotify_auth_state';
+
+var generateRandomString = function(length) {
+  var text = '';
+  var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+  for (var i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+};
+
+router.get('/login', function(req, res, next) {
+  console.log('Recieved login request!!');
+  var state = generateRandomString(16);
+  res.cookie(stateKey, state);
+  
+  // your application requests authorization
+  var scope = 'user-read-private user-read-email';
+  var url1 = 'https://accounts.spotify.com/authorize?';
+  var url2 = querystring.stringify({
+               response_type: 'code',
+               client_id: client_id,
+               scope: scope,
+               redirect_uri: redirect_uri,
+               state: state
+             });
+  var url = url1 + url2;
+
+  console.log('About to redirect!');
+  res.redirect(url);
+  console.log('Finished rerouting');
+});
+
+
+router.get('/callback', function(req, res) {
+  console.log('Callback!');
+  // your application requests refresh and access tokens
+  // after checking the state parameter
+  var code = req.query.code || null;
+  var state = req.query.state || null;
+  var storedState = req.cookies ? req.cookies[stateKey] : null;
+
+  if (state === null || state !== storedState) {
+    res.redirect('/#' +
+      querystring.stringify({
+        error: 'state_mismatch'
+      }));
+  } else {
+    res.clearCookie(stateKey);
+    var authOptions = {
+      url: 'https://accounts.spotify.com/api/token',
+      form: {
+        code: code,
+        redirect_uri: redirect_uri,
+        grant_type: 'authorization_code'
+      },
+      headers: {
+        'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
+      },
+      json: true
+    };
+    request.post(authOptions, function(error, response, body) {
+      if (!error && response.statusCode === 200) {
+        var access_token = body.access_token,
+            refresh_token = body.refresh_token;
+        var options = {
+          url: 'https://api.spotify.com/v1/me',
+          headers: { 'Authorization': 'Bearer ' + access_token },
+          json: true
+        };
+
+        // use the access token to access the Spotify Web API
+        request.get(options, function(error, response, body) {
+          console.log(body);
+        });
+
+        // we can also pass the token to the browser to make requests from there
+        res.redirect('/#' +
+          querystring.stringify({
+            access_token: access_token,
+            refresh_token: refresh_token
+          }));
+      } else {
+        res.redirect('/#' +
+          querystring.stringify({
+            error: 'invalid_token'
+          }));
+      }
+    });
+  }
+});
+
+router.get('/refresh_token', function(req, res) {
+  console.log('Refresh token!');
+  // requesting access token from refresh token
+  var refresh_token = req.query.refresh_token;
+  var authOptions = {
+    url: 'https://accounts.spotify.com/api/token',
+    headers: { 'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64')) },
+    form: {
+      grant_type: 'refresh_token',
+      refresh_token: refresh_token
+    },
+    json: true
+  };
+
+  request.post(authOptions, function(error, response, body) {
+
+    if (!error && response.statusCode === 200) {
+      var access_token = body.access_token;
+      res.send({
+        'access_token': access_token
+      });
+    }
+  });
+});
+
+module.exports = router;
