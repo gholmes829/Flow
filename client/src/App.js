fd546e0eceb72c3ada42bafd95ac3e67d4eca9b9
gholmesss <g.holmes429@gmail.com>
2021-02-01 20:09:57 -0600

    client endpoint connection

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { };
+    }
+
+	render() {
+			return (
+				<div className="App">
+					<header className="App-header">
+						<img src={logo} className="App-logo" alt="logo" />
+						<h1 className="App-title">Welcome to React</h1>
+					</header>
+			<a href="http://catchthatflow.com:9000/spotify/login">Login</a>
+			<a href="http://catchthatflow.com:9000/spotify/playlist">Playlist</a>
+				</div>
+			);
+		}
+	}
 
 export default App;
