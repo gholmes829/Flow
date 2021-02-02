712e4354ce0370d74fbd07795863697512e1a08e
Debian <bitnami@ip-172-26-9-162.ec2.internal>
2021-02-02 01:54:32 +0000

    clean refresh

--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,25 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import logo from './logo.svg';
+import './App.css';
 
-class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { };
-    }
-
-    //callAPI() {
-    //    fetch("http://catchthatflow.com:9000")
-    //        .then(res => res.text())
-    //        .then(res => this.setState({ apiResponse: res }))
-    //        .catch(err => err);
-    //}
-
-    //componentDidMount() {
-    //    this.callAPI();
-    //}
-
-    render() {
-        return (
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <h1 className="App-title">Welcome to React</h1>
-                </header>
-		<a href="http://catchthatflow.com:9000/spotify/login">Login</a>
-		<a href="http://catchthatflow.com:9000/spotify/playlist">Playlist</a>
-            </div>
-        );
-    }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
--- server/app.js
+++ server/app.js
@@ -3,11 +3,9 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var spotifyRouter = require('./routes/spotify');
 
 var app = express();
 
@@ -15,7 +13,6 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -24,7 +21,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/spotify', spotifyRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
