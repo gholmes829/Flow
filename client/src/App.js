604ec05349e29df0114f6dda84894b1eaf9e30ec
gholmesss <g.holmes429@gmail.com>
2021-04-14 22:08:09 -0500

    testing

--- client/src/App.js
+++ client/src/App.js
@@ -37,8 +37,9 @@ class App extends Component {
 					playlists: res.playlists,
 				})
 			console.log("Users playlists:");
-			this.state.playlists.forEach((playlist) => {console.log("Playlist: " + playlist.name)});
+				this.state.playlists.forEach((playlist) => {console.log("Playlist: " + playlist)});
 			})
+			console.log("Selected playlist (not really): " + this.state.playlists);
 			.catch(err => console.log(err));
 	}
 
@@ -46,7 +47,6 @@ class App extends Component {
 	getPlaylistData() {
 		// just getting 1 song
 		let id = this.state.playlists[0].id;
-		console.log("Selected playlist (not really): " + this.state);
 		fetch("http://catchthatflow.com:9000/spotify/playlist/" + id)
 			.then(res => res.json())
 			.then(res => this.setState({selectedPlaylist: res}))
