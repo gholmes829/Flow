63a4e551ae25d3698f5091472d4a1c9b04b78759
gholmesss <g.holmes429@gmail.com>
2021-05-07 01:15:10 -0500

    tweaks

--- server/routes/spotify.js
+++ server/routes/spotify.js
@@ -16,7 +16,7 @@ var refresh_token = '';
 
 // IDs
 var client_id = 'c7cca4bb63634ddf8e6d205c9b23a7b6'; // Your client id
-var client_secret = '849cf7b45dd849ed97ec6990bb6896a6';
+var client_secret = '';
 var redirect_uri = 'http://catchthatflow.com:9000/spotify/callback/';
 var stateKey = 'spotify_auth_state';
 
